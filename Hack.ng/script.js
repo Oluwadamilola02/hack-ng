3bdec102c13ec604907e7a106345742b60f405b1
Oluwadamilola Adeyoyin <108593326+Oluwadamilola02@users.noreply.github.com>
2022-12-12 23:53:28 +0100

    Add files via upload
    https://stellar-longma-2d078d.netlify.app

---
+++ Hack.ng/script.js
@@ -0,0 +1,89 @@
+console.log("Hello world!");
+
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".h1-2");
+console.log(myName);
+console.log(h1);
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+}
+
+///////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value-1");
+animateValue(obj, 1, 3, 950);
+
+// /////////////////////////////////////////////////
+// function animateValue(obj, start, end, duration) {
+//   let startTimestamp = null;
+//   const step = (timestamp) => {
+//     if (!startTimestamp) startTimestamp = timestamp;
+//     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+//     obj.innerHTML = Math.floor(progress * (end - start) + start);
+//     if (progress < 1) {
+//       window.requestAnimationFrame(step);
+//     }
+//   };
+//   window.requestAnimationFrame(step);
+// }
+
+// const object = document.getElementById("value-2");
+// animateValue(obj, 1, 30, 950);
